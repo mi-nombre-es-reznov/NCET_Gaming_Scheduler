60ab5728e93f5eb47ea40b4ac9ab0e7310fc8097
Nicholas Perez-Aguilar <npere017@ucr.edu>
2022-10-04 02:32:11 -0700

    Initial commit

---
+++ main_workflow.py
@@ -0,0 +1,15 @@
+# Main File
+# Created: 4 Oct 2022
+# Modified: 4 Oct 2022
+# Created by: Nicholas Perez-Aguilar
+# NCompEng Technologies
+
+# Imports
+
+# Global Variables
+
+# Functions
+
+# Main Workflow
+if __name__ == '__main__':
+    pass
