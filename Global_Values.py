8d1effd77e4262e23e889954b5066d4b375e98d6
Nicholas Perez-Aguilar <npere017@ucr.edu>
2022-10-06 03:46:03 -0700

    - Added Main Menu options list <- ammendable - Added Months in year with corresponding number

---
+++ Global_Values.py
@@ -0,0 +1,14 @@
+# Global Values
+# Created: 6 Oct 2022
+# Modified: 6 Oct 2022
+# Created by: Nicholas Perez-Aguilar
+# NCompEng Technologies
+
+# Menu Options
+MM_options = ["Create - NCET Gaming - Yearly Folders", "Daily NCET Gaming Tasks"]
+
+# Date Options
+MONTHS = ["1) January", "2) February", "3) March",
+          "4) April" "5) May", "6) June", "7) July",
+          "8) August", "9) September", "10) October",
+          "11) November", "12) December"]
